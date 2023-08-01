14a4586045702e13be8bcd096125580d13ef67e0
nirbhay14 <kashyapnirbhay010@gmail.com>
2023-08-01 16:54:39 +0530

    draft1.4

--- src/components/options/card.jsx
+++ src/components/options/card.jsx
@@ -5,10 +5,10 @@ import Link from "next/link";
 const Card = ({ imgsrc, service }) => {
   return (
     <div className="text-center max-w-[200px] mt-0 mb-4">
-      <div className="image-container rounded-xl overflow-hidden w-[200px] h-[160px] transition-transform duration-300 transform hover:scale-105">
+      <div className="image-container rounded-xl w-36 h-28 overflow-hidden sm:w-[200px] sm:h-[160px] transition-transform duration-300 transform hover:scale-105">
         <Image src={imgsrc} alt="Card Image" layout="fill" objectFit="cover" />
       </div>
-      <div className="mt-2 block max-w-[200px]">
+      <div className="mt-2 block w-36 sm:max-w-[200px]">
         <p className="text-center font-semibold font-[Poppins] text-gray-500 hover:text-black">
           {service}
         </p>
