d019930dde4b21d705defd444cb7e406d56823b7
abhi0sen <senabhisar@gmail.com>
2023-09-24 22:31:50 +0530

    Testimonial: banner is ready

---
+++ src/app/testimonial/page.js
@@ -0,0 +1,12 @@
+import Banner from '@/components/testimonial/Banner'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Banner />
+    </div>
+  )
+}
+
+export default page
