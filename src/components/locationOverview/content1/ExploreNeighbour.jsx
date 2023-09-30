c9c5071a265c9132238224e10cdc2a39f25c4f84
crypticleopard <tarunsingh3443@gmail.com>
2023-09-30 23:54:33 +0530

    completed location page without responsiveness

--- src/components/locationOverview/content1/ExploreNeighbour.jsx
+++ src/components/locationOverview/content1/ExploreNeighbour.jsx
@@ -1,15 +1,32 @@
-import styles from './exploreneighbour.module.css'
 import ExploreNeighbourCard from './ExploreNeighbourCard'
+import { Swiper, SwiperSlide} from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import "swiper/css/pagination";
 
 function ExploreNeighbour() {
+
+  const ExploreData = [
+    <ExploreNeighbourCard key={1} title='Schools & Colleges'/>,
+    <ExploreNeighbourCard key={2} title='Market'/>,
+    <ExploreNeighbourCard key={3} title='Hospitals'/>,
+    <ExploreNeighbourCard key={4} title='Transport'/>,
+    <ExploreNeighbourCard key={5} title='Park'/>,
+    <ExploreNeighbourCard key={6} title='Offices'/>
+  ];
+
   return (
     <div>
-      <ExploreNeighbourCard/>
-      <ExploreNeighbourCard/>
-      <ExploreNeighbourCard/>
-      <ExploreNeighbourCard/>
-      <ExploreNeighbourCard/>
-      <ExploreNeighbourCard/>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          loop ={true}
+          modules={[Pagination]}
+          pagination={{ clickable: true }}
+          className="mySwiper"
+        >
+          {ExploreData.map((dt, index) => (<SwiperSlide key={index} className="pb-12">{dt}</SwiperSlide>))}
+        </Swiper>
     </div>
   )
 }
