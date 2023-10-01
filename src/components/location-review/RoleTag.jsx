d3bcef8dcb6707efd39021a339ef9b40252d5476
Abhishek Chamoli <104618483+AbhishekChamoliDeveloper@users.noreply.github.com>
2023-10-01 09:58:19 +0530

    Merged Branch 01/10/2023

---
+++ src/components/location-review/RoleTag.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const RoleTag = ({ role, onRoleClick, isSelected }) => {
+  return (
+    <div
+      className={`${
+        isSelected ? "border-2 border-blue-800" : ""
+      } bg-blue-200 text-black py-1 px-2 rounded-md inline-block font-semibold `}
+      onClick={() => onRoleClick(role)}
+    >
+      {role}
+    </div>
+  );
+};
+
+export default RoleTag;
