308843e7de4738989db66647773e8a0ee9894e02
crypticleopard <tarunsingh3443@gmail.com>
2024-02-01 16:13:00 +0530

    renamed fnq

--- src/app/location-by-city/[...slug]/page.jsx
+++ src/app/location-by-city/[...slug]/page.jsx
@@ -13,7 +13,7 @@ import ExploreLocalitiesOfCities from "@/components/locationCityWise/ExploreLoca
 import HotspotOfCities from "@/components/locationCityWise/HotspotOfCity";
 import TopPropertyOfCity from "@/components/locationCityWise/TopPropertyOfCity";
 import InterestingReads from "@/components/exploreLocality/ViewAllComponents/InterestingReads";
-import Faq from "@/components/LocationCityWise/FNQ";
+import Faq from "@/components/locationCityWise/Fnqs";
 
 const LocationByCity = () => {
   const [state, city] = useParams().slug;
