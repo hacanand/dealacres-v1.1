cc025ae8ee39bb44a3306c2eeb39c9b627b7b8c6
govindKulk <kulkarnigovind2003@gmail.com>
2024-04-09 23:06:55 +0530

    changes in tailwind config

--- src/components/locationOverview/content2/LocationGuides.jsx
+++ src/components/locationOverview/content2/LocationGuides.jsx
@@ -5,30 +5,23 @@ import { FaExternalLinkAlt } from 'react-icons/fa'
 
 const LocationGuides = ({
     guides
-    
+
 }) => {
     return (
         <div className='font-bold text-2xl py-4 my-4 '>
-            
+
 
             <div className={``}>
 
-            
 
-            <div className={`flex gap-4 w-full flex-wrap min-[920px]:flex-nowrap justify-center min-[920px]:justify-normal   `}>
 
-{/*                         w-[10rem]
-                            h-[15rem]
-                            min-[1220px]:w-[10rem]
-                            min-[1220px]:h-[15rem]
-                            min-[1420px]:w-[12rem]
-                            min-[1420px]:h-[18rem]
-                            min-[1520px]:w-[15rem]
-                            min-[1520px]:h-[21rem] */}
-                {
-                    guides.map((guide, index) => {
-                        return (
-                            <div key={index} className='
+                <div className={`flex gap-4 w-full flex-wrap min-[920px]:flex-nowrap justify-center min-[920px]:justify-normal   `}>
+
+         
+                    {
+                        guides.map((guide, index) => {
+                            return (
+                                <div key={index} className='
                             w-[8rem]
                             sm:w-[10rem]
                             md:w-[11rem]
@@ -38,29 +31,29 @@ const LocationGuides = ({
                             '>
 
 
-                                <GuideCard
-                                variableWidth
-                                title={guide.title} img={guide.img} bgColor={guide.bgColor} textColor={guide.textColor} borderColor={guide.borderColor} key={index} />
-                            </div>
-                        )
-                    })
-                }
+                                    <GuideCard
+                                        variableWidth
+                                        title={guide.title} img={guide.img} bgColor={guide.bgColor} textColor={guide.textColor} borderColor={guide.borderColor} key={index} />
+                                </div>
+                            )
+                        })
+                    }
+
+                </div>
+                <div className=' py-4 '>
+
+                    <span className=' text-blue-500 font-normal text-xl flex items-center justify-center min-[920px]:justify-normal gap-2'>
+                        <Link href='/guides' className=''>View More</Link>
+                        <FaExternalLinkAlt size={20} />
+                    </span>
+
+                </div>
+
+
 
             </div>
-            <div className=' py-4 '>
-
-                <span className=' text-blue-500 font-normal text-xl flex items-center justify-center min-[920px]:justify-normal gap-2'>
-                    <Link href='/guides' className=''>View More</Link>
-                    <FaExternalLinkAlt size={20} />
-                </span>
-
-            </div>
 
 
-
-            </div>
-           
-            
 
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -21,12 +21,32 @@ module.exports = {
         customindigo: "#c7caff",
         custompurple: "#cdabeb",
       },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          "2xl": "1536px",
+        
+      }
     },
+    screens: {
+
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    
+  }
+    
   },
   plugins: [require("daisyui")],
 };
