b30b5f20682401c5c042d99735c1d67056a1926e
anand <anandpromax@gmail.com>
2024-06-17 20:49:20 +0530

    chore: Remove unused "@next/font" dependency and update npm dependencies
    This commit removes the unused "@next/font" dependency from the package.json file. Additionally, it updates the npm dependencies to their latest stable versions, including the "axios" dependency to version 1.7.2.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,21 @@
+"use client";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { usePathname } from "next/navigation";
+import Footer from "../components/footer/Footer";
+import Navbar from "@/components/navbar/Navbar";
+import NavbarSingle from "@/components/single-website/NavbarSingle";
+import FooterSingle from "@/components/single-website/FooterSingle";
+import metadata from "./metadata";
 
-
-'use client';
-
-import './globals.css';
-import { Inter } from 'next/font/google';
-import { usePathname } from 'next/navigation';
-import Footer from '../components/footer/Footer';
-import Navbar from '@/components/navbar/Navbar';
-import NavbarSingle from '@/components/single-website/NavbarSingle';
-import FooterSingle from '@/components/single-website/FooterSingle';
-import metadata from './metadata';
-
-const inter = Inter({ subsets: ['latin'], weight: ['300','400','500','700','900'] });
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "900"],
+});
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
-  const isSingleWebsitePage = pathname === '/single-website';
+  const isSingleWebsitePage = pathname === "/single-website";
 
   return (
     <html lang="en">
@@ -26,7 +26,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         {isSingleWebsitePage ? <NavbarSingle /> : <Navbar />}
         {children}
-        {isSingleWebsitePage ? <FooterSingle /> : <Footer />} 
+        {isSingleWebsitePage ? <FooterSingle /> : <Footer />}
       </body>
     </html>
   );
